48bb908c326c1744fa7f273451d8912f092b964d
iniyan455 <iniyan455@gmail.com>
2020-02-11 14:23:24 +0530

    interceptor added

--- app/src/main/java/com/iniyan/krazybee/NetworkClient/RetrofitClient.java
+++ app/src/main/java/com/iniyan/krazybee/NetworkClient/RetrofitClient.java
@@ -1,5 +1,9 @@
 package com.iniyan.krazybee.NetworkClient;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,11 +11,17 @@ public class RetrofitClient {
     private static Retrofit retrofit;
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
     public static ApiDataService getService() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
         if (retrofit == null) {
             retrofit = new Retrofit
                     .Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(httpClient.build())
                     .build();
         }
         return retrofit.create(ApiDataService.class);
